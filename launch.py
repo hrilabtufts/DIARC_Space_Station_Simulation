dcc128483ed4c60dd15c34867211773b745a6c3c
Matthew McWilliams <hi@mmcwilliams.com>
2024-07-24 10:49:17 -0400

    Collect LSL file after run if exists. Maybe it should be deleted to prevent it being re-copied?

--- launch.py
+++ launch.py
@@ -7,7 +7,7 @@ import subprocess
 import tempfile
 import signal
 import sys
-
+import shutil
 
 parser = ArgumentParser(prog='DIARC Space Station Simulation',
                     description='Script for orchestrating the launch of DIARC, ROS and the Unity Space Station environment for experimentation')
@@ -63,7 +63,7 @@ class DIARCSpaceStation:
 
 	unity_server_bin = './Space_Station_SMM_Server.x86_64'
 
-	required_keys = ['BIN' , 'UNITY_IP', 'DIARC_CONFIG', 'LLM_URL', 'LLM_PORT', 'SERVER', 'SMM', 'NETWORK_PREFIX']
+	required_keys = ['BIN' , 'UNITY_IP', 'DIARC_CONFIG', 'LLM_URL', 'LLM_PORT', 'SMM', 'NETWORK_PREFIX']
 	
 	ros_port = 9090
 	unity_port = 1755
@@ -73,10 +73,6 @@ class DIARCSpaceStation:
 	unity_started = False
 	write_to_log = False
 	log_file = None
-	log_dirs = []
-
-	docker_proc = None
-	unity_proc = None
 
 	def __init__ (self, args) :
 		self._args = args
@@ -105,7 +101,7 @@ class DIARCSpaceStation:
 			envFile = 'default.env'
 		with open(envFile, 'r') as file :
 			for line in file :
-				if line.startswith('#') :
+				if line.startswith('#') or line.strip() == '':
 					continue
 				key, value = line.strip().split('=', 1)
 				value = value.replace('"', '')
@@ -126,13 +122,19 @@ class DIARCSpaceStation:
 		self._line()
 
 	def _overrides(self) :
+		'''Override ENV values with commandline settings'''
 		if 'robots' in self._args and self._args.robots is not None :
 			self._log(f'Overriding ENV value ROBOTS ({self._env["ROBOTS"]}) with {self._args.robots}')
 			self._env['ROBOTS'] = str(self._args.robots)
 
 		if 'smm' in self._args and self._args.smm is not None :
 			self._log(f'Overriding ENV value SMM ({self._env["SMM"]}) with {self._args.smm}')
-			self._env['SMM'] = self._args.smm 
+			self._env['SMM'] = self._args.smm
+
+		if 'noServer' in self._args and self._args.noServer :
+			if 'SERVER' in self._env :
+				self._log(f'Overriding ENV value SERVER with --noServer flag')
+				del self._env['SERVER']
 
 	def _build(self) :
 		'''Build the image. Required prior to run or dev commands.'''
@@ -171,6 +173,7 @@ class DIARCSpaceStation:
 			print(f'[STOP] {line}')
 
 	def _run(self, dev) :
+		'''Start all processes for the run or dev commands'''
 		print(f'{self._args.command.lower()}()')
 
 		timestamp = datetime.now().strftime('%Y_%m_%d-%H_%M_%S')
@@ -183,7 +186,6 @@ class DIARCSpaceStation:
 		if dev :
 			robot_tmpl = './config/docker/docker-compose-robot-dev-headless.yaml.tmpl'
 
-		
 		robots = int(self._env['ROBOTS'])
 		llm_port = int(self._env['LLM_PORT'])
 		ros_port = self.ros_port
@@ -195,10 +197,12 @@ class DIARCSpaceStation:
 			self._log(f'ERROR: Cannot support more than 2 robots with this script')
 			exit(1)
 
-
 		if dev and len(self._args.diarc) != robots :
 			self._log(f'ERROR: Robot count ({robots}) != number of DIARC directories in command ({len(self._args.diarc)})')
 			exit(1)
+
+		if self.write_to_log and 'LSL' in self._env :
+			global_dict['lsl'] = self._env['LSL']
 
 		for i in range(robots) :
 			self.ros_started.append(False)
@@ -267,6 +271,7 @@ class DIARCSpaceStation:
 		self._dockerCompose()
 
 	def _dockerCompose (self) :
+		'''Start the docker compose process and store the subprocess in the global dict'''
 		self._log(f'Launching via {self._env["BIN"]}...')
 		cmd = self._env['BIN'] + ' up --remove-orphans'
 		global_dict['docker_proc'] = subprocess.Popen(cmd.split(' '), stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
@@ -274,25 +279,38 @@ class DIARCSpaceStation:
 			self._parseLog(line)
 
 	def _parseLog (self, line) :
+		'''Parse all lines from docker compose process to determine when ROS is started to start the Unity server'''
 		line = line.decode('ascii').strip()
-		print(line)
-		if not self._args.noServer :
-			if not self.ros_started[0] and 'robot1-' in line and 'process[tilt_shadow_filter-' in line :
-				self.ros_started[0] = True
-			if self.robots == 2 and not self.ros_started[1] and 'robot2-' in line and 'process[tilt_shadow_filter-' in line :
-				self.ros_started[1] = True
+		self._log(line, False)
+		
+		if not self.ros_started[0] and 'robot1-' in line and 'process[tilt_shadow_filter-' in line :
+			self.ros_started[0] = True
 
-			if not self.unity_started and False not in self.ros_started :
-				self._unityServer()
+		if self.robots == 2 and not self.ros_started[1] and 'robot2-' in line and 'process[tilt_shadow_filter-' in line :
+			self.ros_started[1] = True
+
+		if not self.unity_started and False not in self.ros_started :
+			self._unityServer()
 
 	def _unityServer (self) :
+		'''Start the Unity server in a subprocess if in .env file'''
 		self.unity_started = True
-		self._log('STARTING UNITY')
-		cmd = [ self.unity_server_bin ]
-		global_dict['unity_logs'] = os.path.join(self._env['SERVER'], 'Space_Station_SMM_Server_Data/StreamingAssets/Output')
-		global_dict['unity_proc'] = subprocess.Popen(cmd, cwd=self._env['SERVER'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+		if 'SERVER' in self._env :
+			self._log('STARTING UNITY')
+			cmd = [ self.unity_server_bin ]
+			global_dict['unity_logs'] = os.path.join(self._env['SERVER'], 'Space_Station_SMM_Server_Data/StreamingAssets/Output')
+			global_dict['unity_proc'] = subprocess.Popen(cmd, cwd=self._env['SERVER'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+		else :
+			self._log(" *********************************************")
+			self._log(" *                                           *")
+			self._log(" *                                           *")
+			self._log(" *           START UNITY SERVER NOW          *")
+			self._log(" *                                           *")
+			self._log(" *                                           *")
+			self._log(" *********************************************")
 
 	def _additionalServices (self) :
+		'''Generate the docker compose stanza for Kaldi and OpenTTS'''
 		additional_services = self._readLines(self.additional_services_tmpl)
 		additional_services_map = {
 			'NETWORK_PREFIX' : self._env['NETWORK_PREFIX']
@@ -300,6 +318,7 @@ class DIARCSpaceStation:
 		return self._replaceLines(additional_services, additional_services_map)
 
 	def _network (self) :
+		'''Generate the docker compose stanza for the network definition'''
 		network = self._readLines(self.network_tmpl)
 		network_map = {
 			'NETWORK_PREFIX' : self._env['NETWORK_PREFIX']
@@ -307,6 +326,7 @@ class DIARCSpaceStation:
 		return self._replaceLines(network, network_map)
 
 	def _tradeProperties (self, i) :
+		'''Generate the TRADE properties for a hub or spoke from a template'''
 		trade_properties_tmpl = self.trade_properties_hub_tmpl
 		if i > 0 :
 			trade_properties_tmpl = self.trade_properties_spoke_tmpl
@@ -317,6 +337,7 @@ class DIARCSpaceStation:
 		return self._replaceLines(trade_properties, trade_properties_map)
 
 	def _gradleProperties (self) :
+		'''Generate the gradle properties file for a hub or spoke TRADE configuration'''
 		gradle_properties = self._readLines(self.gradle_properties_tmpl)
 		trade_properties_line = self.trade_properties_default
 		if self._env['SMM'] :
@@ -327,6 +348,7 @@ class DIARCSpaceStation:
 		return self._replaceLines(gradle_properties, gradle_properties_map)
 
 	def _rosConfig (self, ros_port, gui) :
+		'''Generate the ROS launch file for starting the PR2'''
 		ros = self._readLines(self.ros_tmpl)
 		ros_map = {
 			'DISPLAY_RVIZ' : str(gui),
@@ -335,6 +357,7 @@ class DIARCSpaceStation:
 		return self._replaceLines(ros, ros_map)
 
 	def _robot (self, dev, gui, robot_name, timestamp, ros_port, ros_tmp, diarc, robot_ip, trade_properties_tmp, unity_port, gradle_properties_tmp, llm_url) :
+		'''Generate the docker compose robot stanza(s)'''
 		tmpl = self.docker_compose_robot_tmpl
 		if not dev and gui :
 			tmpl = self.docker_compose_robot_headless_tmpl
@@ -393,18 +416,21 @@ class DIARCSpaceStation:
 		self._shellPrint(xauth_ls_cmd)
 
 	def _shellPrint(self, cmd) :
+		'''Run a command in a subprocess shell and print output. Intended to be run syncronously'''
 		self._log(f'CMD: {cmd}')
 		proc = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 		for line in proc.stdout :
 			self._log(line)
 
 	def _readLines (self, path) :
+		'''Read all lines in a file and store as a list'''
 		lines = []
 		with open(path, 'r') as file :
 			lines = [line.rstrip() for line in file]
 		return lines
 
 	def _replaceLines(self, lines, replace_map) :
+		'''Replace all instances of a dict key/value pair in all lines of a list'''
 		outLines = []
 		for line in lines :
 			for key in replace_map :
@@ -413,12 +439,14 @@ class DIARCSpaceStation:
 		return outLines
 
 	def _writeLines (self, path, lines) :
+		'''Write all lines from a list to a file with newline delimitation'''
 		with open(path, 'w') as file:
 			for line in lines :
 				file.write(f'{line}\n')
 		self._log(f'Wrote file {path}')
 
 	def _mkdir (self, path) :
+		'''Make a directory if it does not exist'''
 		if not os.path.exists(path) :
 			os.makedirs(path)
 			self._log(f'Created directory {path}')
@@ -426,10 +454,12 @@ class DIARCSpaceStation:
 			self._log(f'Directory {path} exists')
 
 	def _mktemp (self) :
+		'''Create a temporary file and return the path to be written to by another process'''
 		tmp_file, filename = tempfile.mkstemp()
 		return filename
 
 	def _logs (self, timestamp) :
+		'''If output is specified, add all log paths and required variables to be copied to output by the exitGracefully function'''
 		if self._args.output is not None :
 			self.write_to_log = True
 			if os.path.exists(self._args.output) and os.path.isdir(self._args.output) :
@@ -445,16 +475,22 @@ class DIARCSpaceStation:
 			global_dict['diarc_ros_logs'] = timestamp
 			global_dict['output'] = self._args.output
 
-	def _log (self, line) :
-		print(f'[{self._args.command.upper()}] {line}')
+	def _log (self, line, tag = True) :
+		'''Write a log line with optional command name tagged in brackets. If output dir specified write log line to a file there'''
+		if tag :
+			print(f'[{self._args.command.upper()}] {line}')
+		else : 
+			print(line)
 		if self.write_to_log and 'log_file' in global_dict :
 			ts = datetime.now().strftime("%Y %m %d %H:%M:%S.%f")
 			global_dict['log_file'].write(f'[{ts}] {line}\n')
 
 	def _line(self) :
+		'''Draw a line in the terminal'''
 		print('------------------------------')
 
 	def _waitForYes (self) :
+		'''Pause the process to wait for user confirmation'''
 		val = input('Are you ready to continue? (yes/no) : ')
 		if val.strip() == '' or 'y' in val.lower() :
 			self._log('Continuing')
@@ -462,7 +498,8 @@ class DIARCSpaceStation:
 			self._log('Cancelled')
 			exit()
 
-def copy (src, dest) :
+def copyDir (src, dest) :
+	'''Copy one directory to another via cp command'''
 	cmd = ['cp', '-r', src, dest]
 	proc = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 	for line in proc.stdout :
@@ -470,21 +507,31 @@ def copy (src, dest) :
 	print(f'Copied {src} => {dest}')
 
 def copyLogs (timestamp, output) :
+	'''Find directories in ./logs matching the timestamp used to create them and copy into output directory'''
 	dirs = os.listdir('./logs')
 	for dir in dirs :
 		if timestamp in dir :
 			src = os.path.join('./logs', dir)
-			copy(src, output)
+			copyDir(src, output)
 
 def unityLogs (src, output) :
+	'''Copy the logs from the Unity server into the output directory if specified'''
 	dirs = os.listdir(src)
 	dirs.sort(reverse=True)
-	copy(os.path.join(src, dirs[0]), output)
+	copyDir(os.path.join(src, dirs[0]), output)
 
+def lslFile (src, output, timestamp) :
+	dest = os.path.join(output, f'{timestamp}_lsl_data.xdf')
+	if os.path.isfile(src) :
+		shutil.copy2(src, dest)
+		print(f'Copied LSL file {src} => {output}')
+	else :
+		print(f'ERROR: Could not find LSL file {src}')
 
 def exitGracefully () :
+	'''Catch the CTRL-c SIGINT and run all cleanup jobs to shut down subprocesses and copy log files into output directory'''
 	print('Exiting...')
-	print(global_dict)
+	#print(global_dict)
 	if 'unity_proc' in global_dict :
 		global_dict['unity_proc'].kill()
 		print('Killed Unity server')
@@ -500,7 +547,8 @@ def exitGracefully () :
 	if 'unity_logs' in global_dict :
 		unityLogs(global_dict['unity_logs'], global_dict['output'])
 		print('Copied Unity logs')
-
+	if 'lsl' in global_dict :
+		lslFile(global_dict['lsl'], global_dict['output'], global_dict['diarc_ros_logs'])
 
 if __name__ == '__main__':
 	try:
